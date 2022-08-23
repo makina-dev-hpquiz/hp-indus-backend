7a466f18f339e145d97c66fe594c7c9b969a50e5
Makina Dev <makina.informatique@gmail.com>
2022-08-23 23:09:39 +0400

    IncidentPropertiesController ajotu logger

--- src/main/java/com/makina/industrialisation/controllers/IncidentPropertiesController.java
+++ src/main/java/com/makina/industrialisation/controllers/IncidentPropertiesController.java
@@ -1,5 +1,7 @@
 package com.makina.industrialisation.controllers;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,18 +19,23 @@ import com.makina.industrialisation.models.IncidentProperty;
 public class IncidentPropertiesController {
 
 
+	Logger logger = LogManager.getLogger(IncidentPropertiesController.class);
+	
 	@GetMapping(value="/types", produces = MediaType.APPLICATION_JSON_VALUE)
 	public IncidentProperty getTypes() {
+		logger.debug("Appel de l'API getTypes");
 		return IncidentType.getIncidentProperty();
 	}
 
 	@GetMapping(value="/priorities", produces = MediaType.APPLICATION_JSON_VALUE)
 	public IncidentProperty getPriorities() {
+		logger.debug("Appel de l'API getPriorities");
 		return IncidentPriority.getIncidentProperty();
 	}
 
 	@GetMapping(value="/status", produces = MediaType.APPLICATION_JSON_VALUE)
 	public IncidentProperty getStatus() {
+		logger.debug("Appel de l'API getStatus");
 		return IncidentStatus.getIncidentProperty();
 	}	
 }
