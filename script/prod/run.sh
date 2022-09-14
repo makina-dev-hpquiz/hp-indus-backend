9c7199dc8e818238a1ccf8808e8c268e59571965
Makina Dev <makina.informatique@gmail.com>
2022-09-14 15:03:02 +0400

    L'application ne démarre plus une seconde fois si elle est déjà lancé + mise à jour nom hp-qui-latest.apk

--- script/prod/run.sh
+++ script/prod/run.sh
@@ -6,24 +6,32 @@ echo "* Apache tomcat 9.0.55"
 echo "* HP-INDUS-BACKEND"
 echo ;
 
+
+ipconfig | findstr \//C:"IPv4" > ipaddr.txt
+ipaddr=$(grep -o "192.*" ipaddr.txt)
+rm ipaddr.txt
+echo ;
+
 # Lancement de tomcat
 echo "DEMARRAGE DE TOMCAT : "
 bash c:/bin/apache-tomcat-9.0.55/bin/startup.sh
 
-ipconfig | findstr \//C:"IPv4" > ipaddr.txt
-ipaddr=$(grep -o "192.*" ipaddr.txt)
 echo "SERVEUR TOMCAT OK : http://"$ipaddr":8080"
-rm ipaddr.txt
-echo ;
+
 
 # Lancer BDD 
 ##
 
 #Démarrage applicatif
 #HP-Indus-Backend
+pidfile=pid.file
 
-nohup java -jar hp_indus_backend.jar & echo $! > ./pid.file &
-echo "HP-INDUS-BACKEND démarré, il sera dispoible dans quelques instants."
+if [ -f "$pidfile" ]; then   
+    echo "HP-INDUS-BACKEND est déjà démarré."
+else 
+    nohup java -jar hp_indus_backend.jar & echo $! > ./$pidfile &
+    echo "HP-INDUS-BACKEND démarré, il sera dispoible dans quelques instants."
+fi
 
 start "http://"$ipaddr":8080/hp-indus/" 
 
